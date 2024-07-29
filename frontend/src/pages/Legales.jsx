3c6aa56ec15cb081075b61bacad753873da3ca86
wi1dcode <magosdk@icloud.com>
2024-07-29 19:06:22 +0200

    font

--- frontend/src/pages/Legales.jsx
+++ frontend/src/pages/Legales.jsx
@@ -14,12 +14,12 @@ function Legales() {
         />
       </Helmet>
       <NavBar />
-      <section className="container mx-auto px-4 py-8">
+      <section className="container mx-auto px-4 py-8 soleil">
         <h1 className="text-3xl font-bold mb-6">Mentions Légales</h1>
-        <h2 className="text-2xl font-semibold mb-4">
+        <h2 className="text-2xl soleil-book mb-4">
           Éditeur et hébergeur du site
         </h2>
-        <p>
+        <p className="soleil-light">
           Le site theorem-concept.fr est édité par Theorem Concept, domiciliée à
           27 rue stalingrad, 95120, Ermont.
           <br />
@@ -32,28 +32,26 @@ function Legales() {
           Le site theorem-concept.fr est hébergé par IONOS, dont le siège social
           est situé au 7 place de la Gare - 57200 Sarreguemines - France.
         </p>
-        <h2 className="text-2xl font-semibold mt-8 mb-4">
-          Informations légales
-        </h2>
-        <p>
+        <h2 className="text-2xl soleil-book mt-8 mb-4">Informations légales</h2>
+        <p className="soleil-light">
           Theorem Concept est une SAS, immatriculée au Registre du Commerce et
           des Sociétés de Paris sous le numéro ...
           <br />
           Numéro de TVA intracommunautaire : ...
         </p>
-        <h2 className="text-2xl font-semibold mt-8 mb-4">
+        <h2 className="text-2xl soleil-book mt-8 mb-4">
           Propriété intellectuelle
         </h2>
-        <p>
+        <p className="soleil-light">
           Tous les contenus présents sur le site theorem-concept.fr sont
           couverts par le droit d'auteur. Toute reprise est dès lors
           conditionnée à l'accord de l'auteur en vertu de l'article L.122-4 du
           Code de la Propriété Intellectuelle.
         </p>
-        <h2 className="text-2xl font-semibold mt-8 mb-4">
+        <h2 className="text-2xl soleil-book mt-8 mb-4">
           Données personnelles et cookies
         </h2>
-        <p>
+        <p className="soleil-light">
           Ce site collecte des informations sur les pages visitées par les
           utilisateurs via Google Analytics pour des statistiques de visite.
           <br />
@@ -65,10 +63,10 @@ function Legales() {
           par ailleurs s'opposer à l'enregistrement de cookies en employant pour
           cela les fonctionnalités correspondantes sur leur navigateur.
         </p>
-        <h2 className="text-2xl font-semibold mt-8 mb-4">
+        <h2 className="text-2xl soleil-book mt-8 mb-4">
           Limitation de responsabilité
         </h2>
-        <p>
+        <p className="soleil-light">
           Theorem Concept ne saurait être tenu pour responsable des erreurs
           rencontrées sur le site, problèmes techniques, interprétation des
           informations publiées et conséquences de leur utilisation. Par
