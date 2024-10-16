6d38924c1227ee336aaa5b2318dcac6337198cd3
WildCode <mago@localhost.local>
2024-10-16 12:37:31 +0200

    work crud

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,17 @@
-const express = require("express")
-const cors = require("cors")
-const path = require("path")
-const cookieParser = require("cookie-parser")
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const cookieParser = require("cookie-parser");
 
-const guestRouter = require("./routes/guestRouter")
-const userRouter = require("./routes/userRouter")
-const proRouter = require("./routes/proRouter")
-const adminRouter = require("./routes/adminRouter")
-const adminMiddleware = require("./middlewares/adminMiddleware")
-const photosAccessMiddleware = require("./middlewares/photosAccessMiddleware")
+const guestRouter = require("./routes/guestRouter");
+const userRouter = require("./routes/userRouter");
+const proRouter = require("./routes/proRouter");
+const adminRouter = require("./routes/adminRouter");
+const adminMiddleware = require("./middlewares/adminMiddleware");
+const photosAccessMiddleware = require("./middlewares/photosAccessMiddleware");
 // const proMiddleware = require("./middlewares/proMiddleware")
 
-const app = express()
+const app = express();
 
 app.use(
   cors({
@@ -19,28 +19,24 @@ app.use(
     origin: process.env.FRONTEND_URL ?? "http://localhost:3000",
     optionsSuccessStatus: 200,
   })
-)
+);
 
-app.use(express.json())
-app.use(cookieParser())
-// app.use(
-//   "/api/uploads",
-//   express.static(path.join(__dirname, "../public/uploads"))
-// )
-app.use("/api", guestRouter)
-app.use("/api/account", userRouter)
-app.use("/api/pro", proRouter)
-app.use("/api/dashboard", adminMiddleware(), adminRouter)
+app.use("/public", express.static(path.join(__dirname, "..", "public")));
 
-// Serve the public folder for public resources
-// app.use(express.static(path.join(__dirname, "../public/uploads")))
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", guestRouter);
+app.use("/api/account", userRouter);
+app.use("/api/pro", proRouter);
+app.use("/api/dashboard", adminMiddleware(), adminRouter);
+
 app.use(
   "/api/uploads",
   photosAccessMiddleware(),
   express.static(path.join(__dirname, "../public/uploads"))
-)
+);
 
-// Serve REACT APP
-app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")))
+app.use(express.static(path.join(__dirname, "..", "..", "frontend", "dist")));
 
-module.exports = app
+module.exports = app;
